ca2147d633c37b88dd05c9106a11af0169f7a393
Rajiv Nair <rajivnairr@gmail.com>
2018-09-13 21:39:15 +0530

    13092018

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public String firstName;
+        public String lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My full name is "+firstName+" "+lastName);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person john = new Person();
+            var john = new Person();
+            john.firstName = "Rajiv";
+            john.lastName = "Nair";
+            john.Introduce();
+        }
+    }
+}
